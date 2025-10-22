52e4a383eb9ff11cc70f1ace1488e1df8f63416e
greenturtle537 <support@glitchtech.top>
2025-10-22 17:40:07 +0000

    Remove old function

--- utils.js
+++ utils.js
@@ -1,14 +1,3 @@
-function getCharAtPos(x, y) {
-	// Move the cursor to position (x, y)
-	console.gotoxy(x, y);
-	// Calculate the index in the screen buffer
-	var index = (y - 1) * console.screen_columns + (x - 1);
-
-	// Get the character at that position
-	var charAtPosition = console.screen_buf[index];
-	return charAtPosition;
-}
-
 function loadMapToGrid(filename, grid) {
 	var file = new File(filename);
 	if (!file.open("r")) {
